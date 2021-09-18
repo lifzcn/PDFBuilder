de36e259f3ca5ec4e5a24d1d4fb53b6af5cbfdf3
Leonard <leoleechn@qq.com>
2021-09-18 18:36:21 +0800

    进度条
    增加了文件合并进度条，使得文件合并状态更加清晰直观

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 import os
 import sys
+import time
 from PyPDF2 import PdfFileMerger
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtCore import QBasicTimer
 from interface import Ui_Form
 
 
@@ -12,14 +12,14 @@ class mainWindow(QWidget, Ui_Form):
     def __init__(self, parent=None):
         super().__init__()
         self.setupUi(self)
+        self.progressBar_Process.setProperty("value", 0)
         self.pushButton_Select.clicked.connect(self.open)
         self.pushButton_Confirm.clicked.connect(self.combine)
-        
+
     def open(self, parent=None):
         os.startfile("D:\Python_Project\PDFBuilder\source")
 
     def combine(self, parent=None):
-        #str1 = self.textEdit_Source.toPlainText()
         target_path = "D:\Python_Project\PDFBuilder\source"
         pdf_lst = [f for f in os.listdir(target_path) if f.endswith('.pdf')]
         pdf_lst = [os.path.join(target_path, filename) for filename in pdf_lst]
@@ -27,10 +27,14 @@ class mainWindow(QWidget, Ui_Form):
         file_merger = PdfFileMerger()
         for pdf in pdf_lst:
             file_merger.append(pdf)
+        for value in range(100):
+            value += 5
+            time.sleep(0.1)
+            self.progressBar_Process.setProperty("value", value)
         self.lineEdit_Statement.setText("PDF文件合并完成！")
         file_merger.write("{}.pdf".format(str2))
 
-        
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWin = mainWindow()
